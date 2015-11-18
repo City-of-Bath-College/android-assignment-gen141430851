ef83993293e2e4507b025a51b1c0206dfcf6ab21
gen141430851 <gen14143085@COBC33349.CityBathColl.ac.uk>
2015-11-18 11:52:09 +0000

    5

--- MyApplication/app/src/main/java/com/example/gen14143085/myapplication/HighScoreActivity.java
+++ MyApplication/app/src/main/java/com/example/gen14143085/myapplication/HighScoreActivity.java
@@ -4,14 +4,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.paperdb.Paper;
 
 public class HighScoreActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private List<HighScoreObject> highscores;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        Paper.init(this);
+
+        highscores = Paper.book().read("highscores", new ArrayList<HighScoreObject>());
+
+        Toast.makeText(this, "number = " + highscores.size() , Toast.LENGTH_SHORT).show();
+
+        HighscoreAdapter adapter = new HighscoreAdapter(highscores);
+
+        listView.setAdapter(adapter);
+
     }
+
+    private class HighscoreAdapter extends ArrayAdapter<HighScoreObject> {
+
+        public HighscoreAdapter(List<HighScoreObject> items) {
+            super(HighScoreActivity.this, 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(
+                        R.layout.row_highscore, null);
+            }
+
+            //get the highscore object for the row we're looking at
+            HighScoreObject highscore = highscores.get(position);
+
+            return convertView;
+        }// end get view
+
+    }// end adapter class
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
